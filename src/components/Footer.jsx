a4d62c608cee3d9dd769c603173de2c75c0505f8
Salma15S <sal.arb.12a@gmail.com>
2023-08-01 04:12:46 +0100

    first commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,36 +1,22 @@
 import React from 'react';
+import '../../public/css/Liste.css'; 
 
 const Footer = ({ page, totalPages, handlePrevPage, handleNextPage, isLastPage }) => {
   return (
-    <div style={{ 
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center',
-      marginTop: '20px',
-      marginBottom: '20px',
-    }}>
-      <button onClick={handlePrevPage} style={{ 
-        backgroundColor: '#007BFF', 
-        color: 'white', 
-        padding: '8px 16px', 
-        border: 'none', 
-        borderRadius: '5px', 
-        cursor: 'pointer',
-        marginRight: '10px',
-        opacity: page === 1 ? 0.5 : 1,
-        pointerEvents: page === 1 ? 'none' : 'auto'
-      }}>&lt;</button>
-      <p>Page {page}</p>
-      <button onClick={handleNextPage} style={{ 
-        backgroundColor: '#007BFF', 
-        color: 'white', 
-        padding: '8px 16px', 
-        border: 'none', 
-        borderRadius: '5px', 
-        cursor: 'pointer',
-        opacity: isLastPage ? 0.5 : 1,
-        pointerEvents: isLastPage ? 'none' : 'auto'
-      }}>&gt;</button>
+    <div className="footer-container">
+      <button
+        onClick={handlePrevPage}
+        className={`footer-button ${page === 1 ? 'disabled' : ''}`}
+      >
+        &lt;
+      </button>
+      <p className="footer-page-number">Page {page}</p>
+      <button
+        onClick={handleNextPage}
+        className={`footer-button ${isLastPage ? 'disabled' : ''}`}
+      >
+        &gt;
+      </button>
     </div>
   );
 };
